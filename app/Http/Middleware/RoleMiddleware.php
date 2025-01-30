1385f3863e73cde3fc85d811bd17564f9194af7e
JRodriguezN <html5juan@gmail.com>
2025-01-30 12:29:30 -0600

    Se agregaron las protecciones de rutas

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
@@ -15,8 +16,12 @@ class RoleMiddleware
      */
     public function handle(Request $request, Closure $next,$role): Response
     {
-        if (! $request->user() || $request->user()->roles_id_rol !== $role) {
-            /* abort(403, 'No tienes permiso para acceder a esta página.'); */
+        if (!Auth::check()) {
+            return redirect('/login');
+        }
+
+        if (Auth::user()->roles_id_rol != $role) {
+            /* abort(403, 'No tienes permisos para acceder a esta página.'); */
             return redirect('/home');
         }
 
--- routes/web.php
+++ routes/web.php
@@ -117,32 +117,32 @@ Route::get('/campana', function () {
 
 /*--------------*/
 
-Route::resource('cursos/miscursos', CursosController::class)->middleware('auth', 'role:ClienteFree');
-Route::resource('cursos/cursoslist', CursosListController::class)->middleware('auth', 'role:ClienteFree');
-Route::resource('herramientas/nota', NotaController::class)->middleware('auth', 'role:ClienteFree');
-Route::resource('herramientas/metronomo', MetronomoController::class)->middleware('auth', 'role:ClienteFree');
+Route::resource('cursos/miscursos', CursosController::class)->middleware(['auth', 'role:2']);
+Route::resource('cursos/cursoslist', CursosListController::class)->middleware(['auth', 'role:2']);
+Route::resource('herramientas/nota', NotaController::class)->middleware(['auth', 'role:2']);
+Route::resource('herramientas/metronomo', MetronomoController::class)->middleware(['auth', 'role:2']);
 
-Route::resource('userP/herramientas/metronomoP', MetronomoPremiumController::class)->middleware('auth', 'role:ClientePremium');
-Route::resource('userP/herramientas/notaP', NotaPremiumController::class)->middleware('auth', 'role:ClientePremium');
+Route::resource('userP/herramientas/metronomoP', MetronomoPremiumController::class)->middleware(['auth', 'role:3']);
+Route::resource('userP/herramientas/notaP', NotaPremiumController::class)->middleware(['auth', 'role:3']);
 
-Route::resource('servicios/rentaInstrumento', ServicioInstrumentoController::class)->middleware('auth', 'role:ClienteFree');
-Route::resource('servicios/rentaServicio', ServicioController::class)->middleware('auth', 'role:ClienteFree');
+Route::resource('servicios/rentaInstrumento', ServicioInstrumentoController::class)->middleware(['auth', 'role:2']);
+Route::resource('servicios/rentaServicio', ServicioController::class)->middleware(['auth', 'role:2']);
 
-Route::resource('servicios/publicidad', publicidadController::class)->middleware('auth', 'role:ClienteFree');
+Route::resource('servicios/publicidad', publicidadController::class)->middleware(['auth', 'role:2']);
 
-Route::resource('cursos/agregarcurso', AddCursosController::class)->middleware('auth', 'role:Admin');
-Route::resource('cursos/cursoslistAdd', AddCursosListController::class)->middleware('auth', 'role:Admin');
-Route::resource('cursos/acordeon', InstrumentosVientoController::class)->middleware('auth', 'role:Admin');
-Route::resource('viento/acordeon', AcordeonController::class)->middleware('auth', 'role:Admin');
-Route::resource('viento/trompeta', TrompetaController::class)->middleware('auth', 'role:Admin');
-Route::resource('viento/tuba', TubaController::class)->middleware('auth', 'role:Admin');
+Route::resource('cursos/agregarcurso', AddCursosController::class)->middleware(['auth', 'role:1']);
+Route::resource('cursos/cursoslistAdd', AddCursosListController::class)->middleware(['auth', 'role:1']);
+Route::resource('cursos/acordeon', InstrumentosVientoController::class)->middleware(['auth', 'role:1']);
+Route::resource('viento/acordeon', AcordeonController::class)->middleware(['auth', 'role:1']);
+Route::resource('viento/trompeta', TrompetaController::class)->middleware(['auth', 'role:1']);
+Route::resource('viento/tuba', TubaController::class)->middleware(['auth', 'role:1']);
 
 
-Route::resource('cursos/idiofono', IdiofonosController::class)->middleware('auth', 'role:Admin');
-Route::resource('idiofono/campana', CampanaController::class)->middleware('auth', 'role:Admin');
-Route::resource('idiofono/castañuela', CastañuelaController::class)->middleware('auth', 'role:Admin');
-Route::resource('idiofono/xilofono', XilofonoController::class)->middleware('auth', 'role:Admin');
-Route::resource('cursos/instrumentos', InstrumentosController::class)->middleware('auth', 'role:Admin');
+Route::resource('cursos/idiofono', IdiofonosController::class)->middleware(['auth', 'role:1']);
+Route::resource('idiofono/campana', CampanaController::class)->middleware(['auth', 'role:1']);
+Route::resource('idiofono/castañuela', CastañuelaController::class)->middleware(['auth', 'role:1']);
+Route::resource('idiofono/xilofono', XilofonoController::class)->middleware(['auth', 'role:1']);
+Route::resource('cursos/instrumentos', InstrumentosController::class)->middleware(['auth', 'role:1']);
 
 Route::get('/409', function () {
     return view('errors.409');
